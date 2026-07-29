a3c3af904e6d64ee1b1a2275f1ad86c74c326ca8
Szabo Istvan <pisti4395@gmail.com>
2026-07-29 21:13:35 +0200

    asdf

--- calfloat.sh
+++ calfloat.sh
@@ -2,18 +2,24 @@
 #for_window [app_id=\"calendar\"] floating enable, resize set width 300px height 200px, move position 810 0
 #swaymsg "for_window [app_id=\"calendar\"] floating enable, resize set width 300px height 200px, move position 810 0"
 
+#if swaymsg -t get_tree | jq -e ".. | select(.app_id? == \"calendar_popup\")" > /dev/null 2>&1; then
+#    #echo "Window found after ${elapsed}s!"
+#else
+#  swaymsg "exec alacritty -o font.size=15 -t 'calendar' --class calendar_popup -e bash -c 'cal; read -p \"Press Enter to close...\"'"
+#fi
+
 swaymsg "exec alacritty -o font.size=15 -t 'calendar' --class calendar_popup -e bash -c 'cal; read -p \"Press Enter to close...\"'"
 
 #sleep 0.2
 
-while [[ $elapsed -lt 5 ]]; do
-    if swaymsg -t get_tree | jq -e ".. | select(.title? == \"calendar\")" > /dev/null 2>&1; then
-        #echo "Window found after ${elapsed}s!"
-        break
-    fi
-    sleep 0.018  # Small sleep to prevent CPU spinning
-  elapsed=$(( $elapsed + 1 ))
-done
+#while [[ $elapsed -lt 5 ]]; do
+#    if swaymsg -t get_tree | jq -e ".. | select(.title? == \"calendar\")" > /dev/null 2>&1; then
+#        #echo "Window found after ${elapsed}s!"
+#        break
+#    fi
+#    sleep 0.018  # Small sleep to prevent CPU spinning
+#  elapsed=$(( $elapsed + 1 ))
+#done
 
 #swaymsg [title="calendar"] floating enable
 ##echo $(swaymsg resize grow right 1500);
